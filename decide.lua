7ba0fa448e2533f56fa5739db408de1929c49e41
Lorenzo Pistone <blaffablaffa@gmail.com>
2015-01-11 17:05:07 +0100

    Revert "cycle all AS groups when looking for regex"
    This reverts commit e45cf440ff6492b2089fa8132c1f42fdbad9f244.

--- decide.lua
+++ decide.lua
@@ -224,11 +224,9 @@ while true do
       if regex == "" then goto nextcmd end
       regex = pcall(function() (""):match(regex) end) and regex or print("Invalid pattern")
     end
-    for AS in pairs(db) do
-      if type(AS) == "number" then
-        local whois = fetchwhois(AS)
-        if whois and whois:lower():match(regex) then if not inspectAS(AS, db[AS].tag) then break end end
-      end
+    for AS in pairs(db.groups.dunno) do
+      local whois = fetchwhois(AS)
+      if whois and whois:lower():match(regex) then if not inspectAS(AS, db[AS].tag) then break end end
     end
   else return end
 
