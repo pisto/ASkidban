dcfa1dd2edbeba173b6bdb2ab295ef76490eaf85
Lorenzo Pistone <blaffablaffa@gmail.com>
2015-01-25 23:49:08 +0100

    lambda fixes

--- kblibs/lambda.lua
+++ kblibs/lambda.lua
@@ -1,14 +1,10 @@
 local compiled = {}
 
-local function L(code)
-	local cached = compiled[code]
-	if cached then return cached end
-		local L, err, Lr, errr = load("local _, _1, _2, _3, _4, _5, _6, _7, _8, _9 = ..., ... " .. code, "<lambda>")
-		if not L then Lr, errr = load("local _, _1, _2, _3, _4, _5, _6, _7, _8, _9 = ..., ... return " .. code, "<lambda>") end
-		L = L or Lr
-		assert(L, "Cannot compile lambda: " .. tostring(err) .. " | " .. tostring(errr))
-		compiled[code] = L
-	return L
+return function(code)
+  local cached = compiled[code]
+  if cached then return cached end
+  local L, err = load("local _, _1, _2, _3, _4, _5, _6, _7, _8, _9 = ..., ... return " .. code, "<lambda>")
+  if not L then L, err = load("local _, _1, _2, _3, _4, _5, _6, _7, _8, _9 = ..., ... " .. code, "<lambda>") end
+  compiled[code] = assert(L, "Cannot compile lambda: " .. tostring(err))
+  return L
 end
-
-return L
