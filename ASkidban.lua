b9850e2d706ce39b501855665d9411d59eab8e6c
Lorenzo Pistone <blaffablaffa@gmail.com>
2015-02-20 00:28:03 +0100

    move fetchwhois fetchpdb to ASkidban.lua

--- ASkidban.lua
+++ ASkidban.lua
@@ -11,7 +11,7 @@ local oldcd = lfs.currentdir()
 local script, sep = arg[0], package.config:sub(1, 1)
 lfs.chdir(script:match(".*%" .. sep))
 
-local fp, L = require"kblibs.fp", require"kblibs.lambda"
+local fp, L, ip = require"kblibs.fp", require"kblibs.lambda", require"kblibs.ip"
 local map = fp.map
 
 
@@ -51,6 +51,39 @@ while #arg > 0 do
   local a = table.remove(arg, 1)
   if a:sub(1, 1) == '-' then assert(options[a:sub(2)], "Unknown option " .. a)()
   else cmd = assert(commands[a:match("([^%.]+)%.?l?u?a?")], "Unknown command " .. a) end
+end
+
+
+function fetchwhois(AS, force)
+  local data = db[AS][AS]
+  if data.whois and not force then return data.whois end
+  io.write("Fetching whois... ")
+  local whois, err = io.popen("timeout 5 whois AS" .. AS)
+  if not whois then print("Cannot fetch whois for AS" .. AS .. ": " .. err) return end
+  local msg, err = whois:read"*a"
+  local ok = whois:close()
+  if not ok then print("Cannot fetch whois for AS" .. AS .. " (probably interrupted)") return end
+  if not msg then print("Cannot fetch whois for AS" .. AS .. ": " .. err) return end
+  data.whois = msg
+  db:setdata(AS, data)
+  print()
+  return msg
+end
+
+function fetchpdb(AS, force)
+  local data = db[AS][AS]
+  if data.pdb ~= nil and not force then return data.pdb end
+  io.write ("Fetching PeeringDB... ")
+  local whois, err = io.popen("timeout 5 whois -h peeringdb.com AS" .. AS)
+  if not whois then print("Cannot fetch PeeringDB for AS" .. AS .. ": " .. err) return end
+  local msg, err = whois:read"*a"
+  local ok = whois:close()
+  if not ok then print("Cannot fetch PeeringDB for AS" .. AS .. " (probably interrupted)") return end
+  if not msg then print("Cannot fetch PeeringDB for AS" .. AS .. ": " .. err) return end
+  data.pdb = not msg:match"Record not found" and msg or false
+  db:setdata(AS, data)
+  if not data.pdb then print("Not found.") else print() end
+  return data.pdb
 end
 
 
--- decide.lua
+++ decide.lua
@@ -131,39 +131,6 @@ local function prettypdb(pdb)
 end
 
 
-local function fetchwhois(AS, force)
-  local data = db[AS][AS]
-  if data.whois and not force then return data.whois end
-  io.write("Fetching whois... ")
-  local whois, err = io.popen("timeout 5 whois AS" .. AS)
-  if not whois then print("Cannot fetch whois for AS" .. AS .. ": " .. err) return end
-  local msg, err = whois:read"*a"
-  local ok = whois:close()
-  if not ok then print("Cannot fetch whois for AS" .. AS .. " (probably interrupted)") return end
-  if not msg then print("Cannot fetch whois for AS" .. AS .. ": " .. err) return end
-  data.whois = msg
-  db:setdata(AS, data)
-  print()
-  return msg
-end
-
-local function fetchpdb(AS, force)
-  local data = db[AS][AS]
-  if data.pdb ~= nil and not force then return data.pdb end
-  io.write ("Fetching PeeringDB... ")
-  local whois, err = io.popen("timeout 5 whois -h peeringdb.com AS" .. AS)
-  if not whois then print("Cannot fetch PeeringDB for AS" .. AS .. ": " .. err) return end
-  local msg, err = whois:read"*a"
-  local ok = whois:close()
-  if not ok then print("Cannot fetch PeeringDB for AS" .. AS .. " (probably interrupted)") return end
-  if not msg then print("Cannot fetch PeeringDB for AS" .. AS .. ": " .. err) return end
-  data.pdb = not msg:match"Record not found" and msg or false
-  db:setdata(AS, data)
-  if not data.pdb then print("Not found.") else print() end
-  return data.pdb
-end
-
-
 local h = {
   {["<AS>[!-]"] = "Jump to AS number (! => add in database if non existent, - => delete)"},
   {d = "Review dunno"},
